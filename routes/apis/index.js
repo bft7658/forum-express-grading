d11ef532f08b24d1483373ed7099f0616d337f27
bft7658 <bft7658@gmail.com>
2022-07-13 20:42:27 +0800

    feat: api signin add jwt token

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../../config/passport')
 
 const admin = require('./modules/admin')
 const restController = require('../../controllers/apis/restaurant-controller')
+const userController = require('../../controllers/apis/user-controller')
+
 const { apiErrorHandler } = require('../../middleware/error-handler')
 
 router.use('/admin', admin)
+
+// 因為我們不用 cookie-based 驗證了，所以也不需要 Passport 幫我們建立 session，需要把這個功能關掉
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 
 router.get('/restaurants', restController.getRestaurants)
 
