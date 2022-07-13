47b2aa356d5b0e8fb775c172563645f50be1bc9d
bft7658 <bft7658@gmail.com>
2022-07-13 15:07:54 +0800

    feat: add api error handler & delete restaurant api

--- services/admin-services.js
+++ services/admin-services.js
@@ -10,6 +10,16 @@ const adminServices = {
     })
       .then(restaurants => cb(null, { restaurants }))
       .catch(err => cb(err))
+  },
+  deleteRestaurant: (req, cb) => {
+    Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.destroy()
+      })
+      // 在 callback 函式裡，我們把被刪除的那筆餐廳資料(deletedRestaurant)回傳出來給前端。雖然目前不需要用到這筆資料，但我們預留了未來的可能性 (例如：前端有可能會想做一個「刪除成功」的彈跳視窗，讓使用者看見他刪除的資料)。
+      .then(deletedRestaurant => cb(null, { restaurant: deletedRestaurant }))
+      .catch(err => cb(err))
   }
 }
 
