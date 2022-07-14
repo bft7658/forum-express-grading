c05e7358b65cc6cd75b8c2590b502e55651a2f91
bft7658 <bft7658@gmail.com>
2022-07-14 12:22:35 +0800

    feat: modify api unauthorize response

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -1,6 +1,13 @@
 const passport = require('../config/passport')
 
-const authenticated = passport.authenticate('jwt', { session: false })
+const authenticated = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
+    // 在 authenticate 函式內有傳入callback，那就要自行處理驗證成功的地方，而把 user 存進去 req.user 的狀況囉！否則在後續操作 req.user 都會是 undefined
+    req.user = user
+    next()
+  })(req, res, next)
+}
 const authenticatedAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) return next()
   return res.status(403).json({ status: 'error', message: 'permission denied' })
