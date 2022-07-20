c664b02f11ef20ea3e72b51ebe19593138191e0f
bft7658 <bft7658@gmail.com>
2022-07-20 15:59:27 +0800

    feat: modify getUser function by delete unnecessary comment variable

--- services/user-services.js
+++ services/user-services.js
@@ -1,5 +1,5 @@
 // const bcrypt = require('bcryptjs')
-const { User, Comment, Restaurant, Favorite, Like, Followship } = require('../models')
+const { User, Comment, Restaurant } = require('../models')
 // const { imgurFileHandler } = require('../../helpers/file-helpers')
 
 const userServices = {
@@ -16,10 +16,6 @@ const userServices = {
           [{ model: Comment }, 'createdAt', 'DESC']
         ]
       }),
-      // 取得所有評論數，不考慮重複問題
-      Comment.findAndCountAll({
-        where: { userId: req.params.id }
-      }),
       // 將重複留言的餐廳進行篩選，使其合併代表同一間餐廳
       Comment.findAll({
         include: Restaurant,
@@ -29,14 +25,13 @@ const userServices = {
         raw: true
       })
     ])
-      .then(([user, totalComments, commentedRestaurants]) => {
+      .then(([user, commentedRestaurants]) => {
         if (!user) throw new Error("User didn't exist!")
         // 判斷目前登入使用者是否已追蹤該 user 物件
         const isFollowed = req.user.Followings.some(f => f.id === user.id)
         cb(null, {
           user: user.toJSON(),
           commentedRestaurants,
-          totalComments: totalComments.count,
           isFollowed
         })
       })
